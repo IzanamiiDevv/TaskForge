6d7089b058ef41f79cdc3bbe02b52168f0ca6509
IzanamiiDevv <rafaeloli596@gmail.com>
2024-02-23 21:53:24 +0800

    Redirecting Route

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -5,7 +5,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:47123';
 
 const context = [
-  "/weatherforecast",
+  "/API",
 ];
 
 const onError = (err, req, resp, target) => {
